f132bf2deff1b91d6b1dc6781a04a73c540ebb48
gitpk-0 <kell108p@protonmail.com>
2023-07-11 09:39:30 -0400

    activating models

--- quill_scope/settings.py
+++ quill_scope/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'quill_scope_app',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
