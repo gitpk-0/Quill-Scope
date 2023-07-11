a282dbd5d39c3abde14b50254131b41cec8b97ad
gitpk-0 <kell108p@protonmail.com>
2023-07-11 10:05:15 -0400

    entry model

--- quill_scope_app/admin.py
+++ quill_scope_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # import models to be registered
-from .models import Topic
+from .models import Topic, Entry
 
-# manage model thru admin site
+# manage models thru admin site
 admin.site.register(Topic)
+admin.site.register(Entry)
