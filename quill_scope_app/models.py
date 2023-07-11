6a83fa1aa8d802044f14dd2c0b512caa773d5f5d
gitpk-0 <kell108p@protonmail.com>
2023-07-11 10:03:21 -0400

    entry model

--- quill_scope_app/models.py
+++ quill_scope_app/models.py
@@ -6,6 +6,19 @@ class Topic(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
 
-def __str__(self):
-    """Return a string representation of the model"""
-    return self.text
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
+    
+class Entry(models.Model):
+    """Information about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Return a string representation of the model"""
+        return f"{self.text[:50]}..."
