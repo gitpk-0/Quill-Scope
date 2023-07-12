a717e895548683397f641c00c4511d60bde295d0
gitpk-0 <kell108p@protonmail.com>
2023-07-12 10:14:09 -0400

    new topic, new entry, edit entry

--- quill_scope_app/urls.py
+++ quill_scope_app/urls.py
@@ -11,4 +11,27 @@ urlpatterns = [
 
     # All topics page
     path('topics/', views.topics, name='topics'),
+
+    # Detail page for a given topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Page for editing an entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+
+    # # Page for deleting an entry
+    # path('delete_entry/<int:entry_id>/', views.delete_entry, name='delete_entry'),
+
+    # # Page for editing a topic
+    # path('edit_topic/<int:topic_id>/', views.edit_topic, name='edit_topic'),
+
+    # # Page for deleting a topic
+    # path('delete_topic/<int:topic_id>/', views.delete_topic, name='delete_topic'),
+
+
 ]
--- quill_scope_app/views.py
+++ quill_scope_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Entry, Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The home page for Quill Scope"""
@@ -11,3 +12,66 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request, 'quill_scope_app/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request, 'quill_scope_app/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quill_scope_app:topics')
+        
+    # Display a blank or invalid form
+    context = {'form':form}
+
+    return render(request, 'quill_scope_app/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('quill_scope_app:topic', topic_id=topic_id)
+        
+    # Display a blank or invalid form
+    context = {'topic':topic, 'form':form}
+    return render(request, 'quill_scope_app/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quill_scope_app:topic', topic_id=topic.id)
+        
+    # Display a blank or invalid form
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'quill_scope_app/edit_entry.html', context)
