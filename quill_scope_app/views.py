34b5fe9f70562878ad3cef7687d83c8b3a3f9a89
gitpk-0 <kell108p@protonmail.com>
2023-07-13 10:19:00 -0400

    topic protection and association

--- quill_scope_app/views.py
+++ quill_scope_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Entry, Topic
 from .forms import TopicForm, EntryForm
@@ -11,7 +12,7 @@ def index(request):
 @login_required
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics':topics}
     return render(request, 'quill_scope_app/topics.html', context)
 
@@ -19,6 +20,10 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and its entries"""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'quill_scope_app/topic.html', context)
@@ -33,7 +38,9 @@ def new_topic(request):
         # POST data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('quill_scope_app:topics')
         
     # Display a blank or invalid form
@@ -67,6 +74,8 @@ def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry
