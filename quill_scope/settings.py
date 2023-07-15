0861d88ae84e3c8b63a72d0dbf0c76d972a3aab0
gitpk-0 <kell108p@protonmail.com>
2023-07-15 14:47:29 -0400

    icon added

--- quill_scope/settings.py
+++ quill_scope/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
+    # 'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
 ]
 
 ROOT_URLCONF = 'quill_scope.urls'
