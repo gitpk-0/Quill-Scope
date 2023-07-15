550f4dd0be8c6a351becf8ee14d326a54e6bb0c7
gitpk-0 <kell108p@protonmail.com>
2023-07-15 15:17:08 -0400

    icon added

--- quill_scope/urls.py
+++ quill_scope/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView # For favicon
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('quill_scope_app.urls')),
+    path(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     
 ]
